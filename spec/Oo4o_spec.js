7e4984ffa6455f705f6309a01bab7864d8a051cc
bluerabbit.com@gmail.com <bluerabbit.com@gmail.com@9315219a-3ef5-11de-8fef-1f5ae417a2df>
2010-08-31 13:45:16 +0000

    add Oo4o.js spec
    git-svn-id: http://wshf.googlecode.com/svn/trunk@13 9315219a-3ef5-11de-8fef-1f5ae417a2df

---
+++ spec/Oo4o_spec.js
@@ -0,0 +1,17 @@
+(function(){
+eval($.cd(LIB_DIR).load(["Oo4o.js"]));
+with (JSpec('Oo4o.js')) {
+  before_each (function(){
+    this.oo4o = new Oo4o("scott", "tiger", "orcl");
+  });
+  it ('Select文が実行できるか', function(){
+    var ret = this.oo4o.select("select 1 as test from dual");
+    ret[0].test.should_equal("1");
+  });
+
+  it ('バインド変数付Update文が実行できるか', function(){
+    var ret = this.oo4o.executeSQL("UPDATE ENP SET ENAME = ?  where EMPNO = ?", ['name', 7000]);
+    ret.should_equal(1);
+  });
+}
+})();
